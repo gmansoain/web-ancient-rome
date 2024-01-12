ba7375fded10e005094d28060230d793db8ea4d7
Gonzalo Marcos <gmansoain@gmail.com>
2024-01-12 12:26:40 +0000

    Update script.js with connection to Prod DB

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ const emperors_list = document.querySelector(".emperors-list");
 
 loadEmperors();
 async function loadEmperors() {
-  const res = await fetch("http://localhost:8081/api/emperors");
+  //const res = await fetch("http://localhost:8081/api/emperors");
+  const res = await fetch(
+    "https://roman-emperors-api-17ekvx.a671fi.gbr-e1.cloudhub.io/api/emperors"
+  );
   const data = await res.json();
   console.log(data);
   createEmperorsList(data);
