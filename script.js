253c8000bc00bbe18b2c07463275e41be31481e9
Gonzalo Marcos <gmansoain@gmail.com>
2024-01-12 11:23:45 +0000

    First commit

---
+++ script.js
@@ -0,0 +1,25 @@
+console.log("SPQR");
+const emperors_list = document.querySelector(".emperors-list");
+
+loadEmperors();
+async function loadEmperors() {
+  const res = await fetch("http://localhost:8081/api/emperors");
+  const data = await res.json();
+  console.log(data);
+  createEmperorsList(data);
+}
+
+function createEmperorsList(dataArray) {
+  const htmlArr = dataArray.map(
+    (emperor) => `<li class="emperor">
+        <span class="emperor-name">${emperor.name}</span>
+        <p>
+            ${emperor.biography}
+        </p>
+        </li>`
+  );
+  console.log(htmlArr);
+  const html = htmlArr.join("");
+  console.log(html);
+  emperors_list.insertAdjacentHTML("afterbegin", html);
+}
